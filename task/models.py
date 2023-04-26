f3f2dd4effae855cd6a6de34895c1280d8d09055
LilAguh <aguezeochoa@gmail.com>
2023-04-26 09:24:51 -0300

    se crea el modelo de tareas

--- task/models.py
+++ task/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    # Chardfield hace referencia a un string de maximo 200 caracteres
+    title = models.CharField(max_length=200)
+    # Textfield, guarda un texto mas largo, pero no es necesario escribirlo
+    description = models.TextField(blank=True)
+    # BooleanField, hace referencia a un booleano, por defecto false y cambia cuando la tarea este completada
+    done = models.BooleanField(default=False)
+
+    # necesitamos mostrar una data especifica en el panel, no "task data(numero de task)"
+    def __str__(self):
+        # aca pedimos que nos retorne el titulo
+        return self.title
