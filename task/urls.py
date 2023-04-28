768a3705da7737a1502ed5addf9b6afe1d6a550f
LilAguh <aguezeochoa@gmail.com>
2023-04-27 21:20:16 -0300

    se crean las rutas de la api

---
+++ task/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework import routers
+from task import views
+
+# en este archivo se crean las rutas, para posteriormente crear los endpoints
+
+# este router toma todas las rutas y genera las url
+routers = routers.DefaultRouter()
+routers.register(r'task', views.TaskView, 'task')
+
+
+# las rutas necesitan 2 parametros
+# 1 el nombre de la url + api version
+# 2 donde provienen las url generadas por django
+
+urlpatterns = [
+    path('api/v1/', include(routers.urls))
+]
+
+# con esto seria suficiente, ya crea las rutas para las peticiones
+# GET, PUT, DELETE, POST
