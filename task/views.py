c04dc18899008be17508edc291a3f026b61d6cc1
LilAguh <aguezeochoa@gmail.com>
2023-04-27 21:16:53 -0300

    se agrega la respuesta al cliente

--- task/views.py
+++ task/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import TaskSerializer
+from .models import Task
+
 
 # Create your views here.
+# aca van todo aquello que responde al cliente
+# el crud se puede realizar de forma automatica, no es necesario definir los endpoint
+# uno a uno como en express
+
+class TaskView(viewsets.ModelViewSet):
+    # importamos la clases ya creadas
+    serializer_class = TaskSerializer
+    # aca aclaramos que objetos se pueden modificar
+    queryset = Task.objects.all()
