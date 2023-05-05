370abc758bbc8cf6615c67bf5862df437ef4a46b
LilAguh <aguezeochoa@gmail.com>
2023-05-04 21:03:23 -0300

    se agregan las rutas y la navegacion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.scss'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { TaskPage } from './pages/TaskPage'
+import { TaskFormPage } from './pages/TaskFormPage'
+import { Navigation } from './components/Navigation'
 
 function App() {
 
   return (
     <div className='App'>
-      <h1>Hola mundo</h1>
+      <BrowserRouter>
+
+        <Navigation/>
+
+        <Routes>
+          <Route path='/' element={<Navigate to='/tasks'/>}/>
+          <Route path='/tasks' element={<TaskPage/>}/>
+          <Route path='/tasks-create' element={<TaskFormPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
